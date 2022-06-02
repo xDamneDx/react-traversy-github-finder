1fb9e315f496f0d7dfc5970d3dc629e65a463be8
xDamneDx <xdamnedx@gmail.com>
2022-06-02 13:37:54 +0300

    Added components & routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Components:
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
