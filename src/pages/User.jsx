108ce5de8b8e63dea14d019efb3c58a5fd79303d
xDamneDx <xdamnedx@gmail.com>
2022-06-08 10:06:31 +0300

    Added markup for a couple stats

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,4 +1,4 @@
-import { FaCodepen, FaStore, FaUserFriends, FaUser } from "react-icons/fa";
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
 import { useEffect, useContext } from "react";
 import { useParams, Link } from "react-router-dom";
 import GithubContext from "../context/github/GithubContext";
@@ -45,7 +45,7 @@ function User() {
           </div>
           <div className="grid grid-cols-1 mb-8 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 md:gap-8">
             <div className="mb-6 custom-card-image md:mb-0">
-              <div className="flex rounded-lg shadow-xl card image-full">
+              <div className="rounded-lg shadow-xl card image-full">
                 <figure>
                   <img src={avatar_url} alt="User avatar" />
                 </figure>
@@ -75,6 +75,80 @@ function User() {
                   >
                     Visit Github Profile
                   </a>
+                </div>
+              </div>
+
+              <div className="w-full rounded-lg shadow-md bg-base-100 stats">
+                {location && (
+                  <div className="stat">
+                    <div className="stat-title text-md">Location</div>
+                    <div className="text-lg stat-value">{location}</div>
+                  </div>
+                )}
+                {blog && (
+                  <div className="stat">
+                    <div className="stat-title text-md">Website</div>
+                    <div className="text-lg stat-value">
+                      <a href={blog} target="_blank" rel="noreferrer">
+                        {blog}
+                      </a>
+                    </div>
+                  </div>
+                )}
+                {twitter_username && (
+                  <div className="stat">
+                    <div className="stat-title text-md">Twitter</div>
+                    <div className="text-lg stat-value">
+                      <a
+                        href={`https://twitter.com/${twitter_username}`}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {twitter_username}
+                      </a>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
+            <div className="grid grid-cols-1 md:grid-cols-3">
+              <div className="stat">
+                <div className="stat-figure text-secondary">
+                  <FaUsers className="text-3xl md:text-5xl" />
+                </div>
+                <div className="pr-5 stat-title">Followers</div>
+                <div className="pr-5 text-3xl stat-value md:text-4xl">
+                  {followers}
+                </div>
+              </div>
+              <div className="stat">
+                <div className="stat-figure text-secondary">
+                  <FaUserFriends className="text-3xl md:text-5xl" />
+                </div>
+                <div className="pr-5 stat-title">Following</div>
+                <div className="pr-5 text-3xl stat-value md:text-4xl">
+                  {following}
+                </div>
+              </div>
+              <div className="stat">
+                <div className="stat-figure text-secondary">
+                  <FaCodepen className="text-3xl md:text-5xl" />
+                </div>
+                <div className="pr-5 stat-title">Public Repos</div>
+                <div className="pr-5 text-3xl stat-value md:text-4xl">
+                  {public_repos}
+                </div>
+              </div>
+              <div className="stat">
+                <div className="stat-figure text-secondary">
+                  <FaStore className="text-3xl md:text-5xl" />
+                </div>
+                <div className="pr-5 stat-title">Public Gists</div>
+                <div className="pr-5 text-3xl stat-value md:text-4xl">
+                  {public_gists}
                 </div>
               </div>
             </div>
