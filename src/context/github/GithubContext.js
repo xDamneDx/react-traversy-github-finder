05baeecbc88ec85d39e3e6401f267a19ba967f2e
xDamneDx <xdamnedx@gmail.com>
2022-06-08 10:41:15 +0300

    Created getUserRepos function

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,7 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
   };
 
@@ -43,15 +44,33 @@ export const GithubProvider = ({ children }) => {
     }
   };
 
+  // Get user repos:
+  const getUserRepos = async (login) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    });
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`
+    );
+    const data = await response.json();
+    dispatch({ type: "GET_REPOS", payload: data });
+  };
+
   return (
     <GithubContext.Provider
       value={{
         users: state.users,
         user: state.user,
+        repos: state.repos,
         loading: state.loading,
         searchUsers,
         getUser,
         clearUsers,
+        getUserRepos,
       }}
     >
       {children}
