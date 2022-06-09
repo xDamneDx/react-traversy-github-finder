d4aac3ec8a92c538330b0c082da4f48acdb90d92
xDamneDx <xdamnedx@gmail.com>
2022-06-09 10:47:15 +0300

    Added getUser function

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -11,3 +11,15 @@ export const searchUsers = async (text) => {
 
   return items;
 };
+
+// Get single user:
+export const getUser = async (login) => {
+  const response = await fetch(`${GITHUB_URL}/users/${login}`);
+
+  if (response.status === 404) {
+    window.location = "/notfound";
+  } else {
+    const data = await response.json();
+    return data;
+  }
+};
