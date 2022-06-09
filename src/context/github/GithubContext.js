aea93b64d60b2c50071679fa9c160b777f87e3f4
xDamneDx <xdamnedx@gmail.com>
2022-06-09 11:10:33 +0300

    Removed getUserRepos

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -3,8 +3,6 @@ import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
-const GITHUB_URL = "https://api.github.com";
-
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
@@ -14,24 +12,7 @@ export const GithubProvider = ({ children }) => {
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
-  const setLoading = () => dispatch({ type: "SET_LOADING" });
   const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
-
-  // Get user repos:
-  const getUserRepos = async (login) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      sort: "created",
-      per_page: 10,
-    });
-
-    const response = await fetch(
-      `${GITHUB_URL}/users/${login}/repos?${params}`
-    );
-    const data = await response.json();
-    dispatch({ type: "GET_REPOS", payload: data });
-  };
 
   return (
     <GithubContext.Provider
@@ -39,7 +20,6 @@ export const GithubProvider = ({ children }) => {
         ...state,
         dispatch,
         clearUsers,
-        getUserRepos,
       }}
     >
       {children}
