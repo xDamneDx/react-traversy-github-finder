9d7b732035ae909215edee1097f9b2eba7bf7f24
xDamneDx <xdamnedx@gmail.com>
2022-06-09 10:14:54 +0300

    Removed searchUsers

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -17,19 +17,6 @@ export const GithubProvider = ({ children }) => {
   const setLoading = () => dispatch({ type: "SET_LOADING" });
   const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
 
-  // Get search results:
-  const searchUsers = async (text) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      q: text,
-    });
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
-    const { items } = await response.json();
-    dispatch({ type: "GET_USERS", payload: items });
-  };
-
   // Get single user:
   const getUser = async (login) => {
     setLoading();
@@ -63,11 +50,8 @@ export const GithubProvider = ({ children }) => {
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        user: state.user,
-        repos: state.repos,
-        loading: state.loading,
-        searchUsers,
+        ...state,
+        dispatch,
         getUser,
         clearUsers,
         getUserRepos,
